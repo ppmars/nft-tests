2b52e8fa19afe88416485e49235965c55ef63302
Gabriel Ruoff <geruoff@gmail.com>
2022-01-08 00:36:26 -0500

    plt labels

--- openSea-monitor.py
+++ openSea-monitor.py
@@ -1,36 +1,31 @@
-import matplotlib
+import OpenSea
 import matplotlib.pyplot as plt
 from datetime import datetime
-from web3 import Web3
-
 import numpy as np
-
 plt.style.use('ggplot')
-import OpenSea
 
-collection_name = "boredapeyachtclub"
-collection_contract_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+# collection name is the only input
+collection_name = "veefriends"
 
 OpS = OpenSea.OpenSea()
-# collection = OpS.getCollection(collection_name)
-# collection_assets = OpS.getAssets(1, 0, asset_contract=collection.ERC721Address)
-#
-# for ape in collection_assets:
+collection = OpS.get_collection(collection_name)
+events = OpS.get_events(1200, asset_contract_address=collection.ERC721Address, event_type="successful")
+
+# y
+sale_prices = [float(event.total_price) / 1000000000000000000 for event in events]
+# x
+dates = [datetime.strptime(event.created_date, "%Y-%m-%dT%H:%M:%S.%f") for event in events]
+# x must be integers for np.polyfit()
+x = range(len(sale_prices))
+
+plt.scatter(x, sale_prices)
+plt.plot(np.unique(x), np.poly1d(np.polyfit(x, sale_prices, 1))(np.unique(x)), color="blue")
+plt.title("Last {} {} Sales".format(len(x), collection_name))
+plt.ylabel("Eth")
+plt.show()
+
+
+# for ape in OpenSea.get_assets(name=collection_name):
 #     # print(ape.token_id, ape.current_price)
 #     buyOrder = ape.buy("0xjhjdjfg")
 #     print(buyOrder)
-
-events = OpS.get_events(300, asset_contract_address=collection_contract_address, event_type="successful")
-print("got " + str(len(events)) + " events")
-sale_prices = []
-dates = []
-for event in events:
-    print(event.jsonData)
-    dates.append(datetime.strptime(event.created_date, "%Y-%m-%dT%H:%M:%S.%f"))
-    sale_prices.append(float(event.total_price))
-sale_prices = [x / 1000000000000000000 for x in sale_prices]
-
-x = range(len(sale_prices))
-plt.scatter(x, sale_prices)
-plt.plot(np.unique(x), np.poly1d(np.polyfit(x, sale_prices, 1))(np.unique(x)), color="blue")
-plt.show()
